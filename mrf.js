f9435efb7bef06c73a944c67d8ed304c46bc9683
vijayabharathy <96302402+vijayabharathy16@users.noreply.github.com>
2021-12-26 20:52:02 +0530

    added some one

---
+++ mrf.js
@@ -0,0 +1,43 @@
+const scores = [
+    {
+      marks: 32,
+      name: "Yvette Merritt"
+    },
+    {
+      marks: 57,
+      name: "Lillian Ellis"
+    },
+    {
+      marks: 22,
+      name: "Mccall Carter"
+    },
+    {
+      marks: 21,
+      name: "Pate Collier"
+    },
+    {
+      marks: 91,
+      name: "Debra Beard"
+    },
+    {
+      marks: 75,
+      name: "Nettie Hancock"
+    },
+    {
+      marks: 20,
+      name: "Hatfield Hodge"
+    }
+  ]; 
+
+ //----------------task -1------------------//
+// const total = scores.map((score) =>score.marks);
+// console.log(total);
+// const output = total.reduce((sum,curr) => sum + curr,0);
+// console.log(output);
+// const average = output/total.length;
+// console.log(average);
+//-----------------task -2---------------------//
+// const topmark = scores.filter((score) =>score.marks>90);
+// console.log(topmark);
+//  const getname =topmark.map((score) => score.name);
+//  console.log(getname);

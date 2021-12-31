56df109aaf11533407fc9e70b58842f593a26cc4
vijayabharathy <96302402+vijayabharathy16@users.noreply.github.com>
2021-12-31 19:35:44 +0530

    08-task added

---
+++ cls-uber.js
@@ -0,0 +1,60 @@
+//---------------------- 3.person class hold on the detail----------------------------------------// 
+// class Account {
+//     constructor (name,accno,balance){
+//       this.name = name;
+//       this.accno = accno;
+//       this.balance = balance;
+//     }
+//     getbalance(){
+//       return `the balance is: ${this.balance}`;
+//     }
+//     deposite(amount){
+//       this.balance = this.balance + amount;
+//       return this.getbalance();
+//     }
+//   }
+// const vijay = new Account ("vijay", 150, 10000000);
+// const vani = new Account ("vani", 151, 2000000);
+// const ganesh = new Account ("ganesh", 152 , 80000);
+// console.log(vijay.deposite(10000));
+// console.log(vani.deposite(20000));
+// console.log(ganesh.deposite(10000));  
+
+
+
+//-----------------------4.calculate uber price-----------------------------------------------------//
+// var uberClientId = "dvgcjns"
+//     , uberServerToken = "hdvs16vgdv";
+//  var userstate
+//     , usercountry
+//    partystate = chennai
+//   , partycountry = canada;
+
+
+
+// navigator.geolocation.watchPosition(function(position) {
+    
+//     userstate= area.track.chennai
+//     ,usercountry = area.track.canada; 
+
+  
+//      getEstimatesForUserLocation(userstate, usercountry);
+// });
+
+// function getEstimatesForUserLocation(state,country) {
+//   $.ajax({
+//     url: "https://api.uber.com/v1/estimates/price",
+//     headers: {
+//         Authorization: "karanaka" + uberServerToken
+//     },
+//     data: { 
+//       start_state: state,
+//       start_country: country,
+//       end_state: partystate,
+//       end_country: partycountry
+//     },
+//     success: function(result) {
+//       console.log(result);
+//     }
+//   });
+// } 
